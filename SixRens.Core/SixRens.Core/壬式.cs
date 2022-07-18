c532c5be93864fc2998de6fa8e7b7dfbc52e73e2
yueyinqiu <lujiaqi2003@126.com>
2022-07-18 21:42:17 +0800

    根据api更新

--- SixRens.Core/SixRens.Core/壬式.cs
+++ SixRens.Core/SixRens.Core/壬式.cs
@@ -28,6 +28,16 @@ namespace SixRens.Core
 
         public IReadOnlyList<I占断参考> 占断参考 { get; }
 
+        public I地盘插件 所用地盘插件 { get; }
+        public I天盘插件 所用天盘插件 { get; }
+        public I四课插件 所用四课插件 { get; }
+        public I三传插件 所用三传插件 { get; }
+        public I天将插件 所用天将插件 { get; }
+        public I年命插件 所用年命插件 { get; }
+        public IReadOnlyCollection<I神煞插件> 所用神煞插件 { get; }
+        public IReadOnlyCollection<I课体插件> 所用课体插件 { get; }
+        public IReadOnlyCollection<I参考插件> 所用参考插件 { get; }
+
         public 壬式(I年月日时信息 年月日时,
             本命信息? 课主本命,
             IEnumerable<本命信息> 对象本命,
@@ -41,13 +51,23 @@ namespace SixRens.Core
             IEnumerable<I课体插件> 课体插件,
             IEnumerable<I参考插件> 参考插件)
         {
+            所用地盘插件 = 地盘插件;
+            所用天盘插件 = 天盘插件;
+            所用四课插件 = 四课插件;
+            所用三传插件 = 三传插件;
+            所用天将插件 = 天将插件;
+            所用年命插件 = 年命插件;
+            所用神煞插件 = Array.AsReadOnly(神煞插件.ToArray());
+            所用课体插件 = Array.AsReadOnly(课体插件.ToArray());
+            所用参考插件 = Array.AsReadOnly(参考插件.ToArray());
+
             this.年月日时 = 年月日时.生成年月日时();
 
             var 基础盘 = new 地支盘();
 
             try
             {
-                地盘 = 地盘插件.获取地盘(
+                地盘 = 所用地盘插件.获取地盘(
                    this.年月日时, 基础盘);
             }
             catch (起课失败异常 异常)
@@ -57,7 +77,7 @@ namespace SixRens.Core
 
             try
             {
-                天盘 = 天盘插件.获取天盘(
+                天盘 = 所用天盘插件.获取天盘(
                     this.年月日时, 基础盘, 地盘);
             }
             catch (起课失败异常 异常)
@@ -67,7 +87,7 @@ namespace SixRens.Core
 
             try
             {
-                四课 = 四课插件.获取四课(
+                四课 = 所用四课插件.获取四课(
                     this.年月日时, 基础盘, 地盘, 天盘);
             }
             catch (起课失败异常 异常)
@@ -77,7 +97,7 @@ namespace SixRens.Core
 
             try
             {
-                三传 = 三传插件.获取三传(
+                三传 = 所用三传插件.获取三传(
                     this.年月日时, 基础盘, 地盘, 天盘, 四课);
             }
             catch (起课失败异常 异常)
@@ -87,7 +107,7 @@ namespace SixRens.Core
 
             try
             {
-                天将盘 = 天将插件.获取天将盘(
+                天将盘 = 所用天将插件.获取天将盘(
                     this.年月日时, 基础盘, 地盘, 天盘, 四课, 三传);
             }
             catch (起课失败异常 异常)
@@ -100,7 +120,7 @@ namespace SixRens.Core
                 I年命 年命;
                 try
                 {
-                    年命 = 年命插件.获取年命(
+                    年命 = 所用年命插件.获取年命(
                         this.年月日时, 基础盘, 地盘, 天盘, 四课, 三传, 天将盘,
                         课主本命.性别, 课主本命.本命);
                 }
@@ -118,7 +138,7 @@ namespace SixRens.Core
             {
                 对象年命 =
                     from 本命 in 对象本命
-                    let 年命 = 年命插件.获取年命(
+                    let 年命 = 所用年命插件.获取年命(
                         this.年月日时, 基础盘, 地盘, 天盘, 四课, 三传, 天将盘,
                         本命.性别, 本命.本命)
                     select new 年命信息(年命.性别, 年命.本命, 年命.行年);
@@ -130,38 +150,37 @@ namespace SixRens.Core
             this.对象年命 = Array.AsReadOnly(对象年命.ToArray());
 
             Dictionary<string, List<EarthlyBranch>> 神煞字典 = new();
-            foreach (var 插件 in 神煞插件)
+            foreach (var 插件 in 所用神煞插件)
             {
-                IEnumerable<I神煞> 插件神煞表;
                 try
                 {
-                    插件神煞表 = 插件.获取神煞(
-                        this.年月日时, 基础盘, 地盘, 天盘, 四课, 三传, 天将盘,
-                        课主年命, this.对象年命);
+                    foreach (var 神煞 in 插件.支持的神煞)
+                    {
+                        if (!神煞字典.ContainsKey(神煞.神煞名))
+                            神煞字典.Add(神煞.神煞名, new(1));
+
+                        var 神煞内容 = 插件.获取神煞(
+                                this.年月日时, 基础盘, 地盘, 天盘, 四课, 三传, 天将盘,
+                                课主年命, this.对象年命, 神煞);
+                        神煞字典[神煞.神煞名].AddRange(神煞内容.所在神);
+                    }
                 }
                 catch (起课失败异常 异常)
                 {
                     throw new 起课失败异常($"获取神煞失败（插件：{插件.插件识别码}）：{异常.Message}", 异常);
                 }
-                foreach (var 神煞 in 插件神煞表)
-                {
-                    if (!神煞字典.ContainsKey(神煞.神煞名))
-                        神煞字典.Add(神煞.神煞名, new());
-                    if (神煞.所在神 is not null)
-                        神煞字典[神煞.神煞名].AddRange(神煞.所在神);
-                }
             }
-            var 神煞表 = new List<神煞>(神煞字典.Count);
-            foreach (var (神煞名, 所在神) in 神煞字典)
-            {
-                var 在神 = 所在神.ToArray();
-                if (在神.Length > 0)
-                    神煞表.Add(new 神煞(神煞名, Array.AsReadOnly(在神)));
-            }
-            神煞 = new ReadOnlyCollection<神煞>(神煞表);
+
+            var 神煞表 =
+                from 神煞名所在神 in 神煞字典
+                let 神煞名 = 神煞名所在神.Key
+                let 所在神 = 神煞名所在神.Value
+                where 所在神.Count > 0
+                select new 神煞(神煞名, Array.AsReadOnly(所在神.ToArray()));
+            神煞 = Array.AsReadOnly(神煞表.ToArray());
 
             HashSet<I课体> 课体 = new();
-            foreach (var 插件 in 课体插件)
+            foreach (var 插件 in 所用课体插件)
             {
                 IEnumerable<I课体> 插件课体;
                 try
@@ -180,24 +199,27 @@ namespace SixRens.Core
             this.课体 = Array.AsReadOnly(课体.ToArray());
 
 
-            HashSet<I占断参考> 占断参考 = new();
-            foreach (var 插件 in 参考插件)
+            List<I占断参考> 占断参考 = new();
+            foreach (var 插件 in 所用参考插件)
             {
-                IEnumerable<I占断参考> 插件参考;
+                IEnumerable<占断参考> 插件参考;
                 try
                 {
-                    插件参考 = 插件.生成占断参考(
-                        this.年月日时, 基础盘, 地盘, 天盘, 四课, 三传, 天将盘,
-                        课主年命, this.对象年命, 神煞, this.课体);
+                    插件参考 =
+                        from 参考题目 in 插件.支持的占断参考
+                        let 参考内容 = 插件.生成占断参考(
+                            this.年月日时, 基础盘, 地盘, 天盘, 四课, 三传, 天将盘,
+                            课主年命, this.对象年命, 神煞, this.课体, 参考题目)
+                        where 参考内容.内容 is not null
+                        select new 占断参考(参考题目.题目, 参考内容.内容);
                 }
                 catch (起课失败异常 异常)
                 {
                     throw new 起课失败异常($"获取参考失败（插件：{插件.插件识别码}）：{异常.Message}", 异常);
                 }
-                foreach (var 单参考 in 插件参考)
-                    _ = 占断参考.Add(new 占断参考(单参考.题目, 单参考.内容));
+                占断参考.AddRange(插件参考);
             }
-            this.占断参考 = Array.AsReadOnly(占断参考.ToArray());
+            this.占断参考 = new ReadOnlyCollection<I占断参考>(占断参考);
         }
 
         public 天将 取所乘将(EarthlyBranch 上者)
