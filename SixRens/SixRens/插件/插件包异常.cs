5e592cadb8c5ab5272b7589c14cccc2fa350abfa
yueyinqiu <lujiaqi2003@126.com>
2022-07-18 20:20:09 +0800

    添加插件包功能

---
+++ SixRens/SixRens/插件/插件包异常.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixRens.插件
+{
+    [Serializable]
+    public class 插件包读取异常 : Exception
+    {
+        public 插件包读取异常() { }
+        public 插件包读取异常(string message) : base(message) { }
+        public 插件包读取异常(string message, Exception inner) : base(message, inner) { }
+        protected 插件包读取异常(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
